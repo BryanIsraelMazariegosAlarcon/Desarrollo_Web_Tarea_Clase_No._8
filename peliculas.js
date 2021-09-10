2c8f5e731307236c0fdb68ce8b8f20389499cb86
BryanIsraelMazariegosAlarcón <bryanisrael76@yahoo.com>
2021-09-10 15:18:51 -0600

    Actualización No. 7

--- peliculas.js
+++ peliculas.js
@@ -1,4 +1,4 @@
-export function peliculas (api1){            
+export function peliculas (api1){
     fetch(api1).then((resultado) => {
         if (resultado.status == 200) {
             resultado.json().then((datos) => {
@@ -6,8 +6,8 @@ export function peliculas (api1){
                 datos.results.forEach(gen => {
                     html += `<div>
                             <p>${gen.title}</p>
-                            <p>Fecha de lanzamiento: ${gen.release_date}</p> 
-                                <img src="https://image.tmdb.org/t/p/w500${gen.poster_path}"/>  
+                            <p>Fecha de lanzamiento: ${gen.release_date}</p>
+                                <img src="https://image.tmdb.org/t/p/w500${gen.poster_path}"/>
                             </div>`;
                 })
                 document.getElementById("peliculas").innerHTML = html;
--- serie.js
+++ serie.js
@@ -1,4 +1,4 @@
-export function serie (api1){            
+export function serie (api1){
     fetch(api1).then((resultado) => {
         if (resultado.status == 200) {
             resultado.json().then((datos) => {
@@ -6,11 +6,11 @@ export function serie (api1){
                 datos.results.forEach(gen => {
                     html += `<div>
                             <p>${gen.name}</p>
-                            <p>Fecha de lanzamiento: ${gen.first_air_date}</p> 
+                            <p>Fecha de lanzamiento: ${gen.first_air_date}</p>
                                 <img src="https://image.tmdb.org/t/p/w500${gen.poster_path}"/>
                             </div>`;
                 })
-                document.getElementById("peliculas").innerHTML = html;                
+                document.getElementById("peliculas").innerHTML = html;
             });
         }
     });
